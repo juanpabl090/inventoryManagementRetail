d3ddeb55c9e0c81e635c63fd143d2770c81eb759
Juan Pablo Montoya Hernandez <juanrona12@gmail.com>
2025-07-07 15:07:29 -0600

    remove TODO comments regarding saleDetail and sale amount calculations

--- src/main/java/com/example/inventoryManagementRetail/InventoryManagementRetailApplication.java
+++ src/main/java/com/example/inventoryManagementRetail/InventoryManagementRetailApplication.java
@@ -7,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InventoryManagementRetailApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryManagementRetailApplication.class, args);
-        //TODO: Revisar porque no esta bien el amount en saleDetail (precio sin descuento por cada producto)
-        //TODO: revisar si retorna bien el amount en sale (precio total de todos lor productos con descuento incluido)
     }
 }
