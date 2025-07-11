33ddd9f05978ffa9e368811f79b0581e25f4c809
Juan Pablo Montoya Hernandez <juanrona12@gmail.com>
2025-07-11 17:40:10 -0600

    add: jwt security

---
+++ src/main/java/com/example/inventoryManagementRetail/model/User.java
@@ -0,0 +1,30 @@
+package com.example.inventoryManagementRetail.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String userName;
+    private String password;
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+}
